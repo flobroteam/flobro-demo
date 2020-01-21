0a509723f176723c693d605959ca61f9967c807a
Whatley, Cesar <julceswhat@gmail.com>
2020-01-20 19:34:20 -0500

    Breaking cloud function for automation.

--- functions/index.js
+++ functions/index.js
@@ -9,15 +9,26 @@ const API_KEY = functions.config().sendgrid.key;
 sgMail.setApiKey(API_KEY);
 
 exports.emailMessage = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        const msg = {
-            to: ['info@flobrofitness.com', 'julceswhat@gmail.com'],
-            from: 'test@flobrodemo.com',
-            subject: 'New Program Request',
-            templateId: 'd-d9f158ed05b44a799880e0765df62c57',
-            dynamic_template_data: req.body,
-        };
-        sgMail.send(msg)
-        res.status(200).send({status: 'success'});
+    return cors(req, res, async() => {
+        await sendApplicationEmail(req.body);
+
+
+
+        res.status(200).send({ status: 'success' });
     });
 });
+
+async function sendApplicationEmail(body) {
+
+    const msg = {
+        to: ['info@flobrofitness.com', 'julceswhat@gmail.com'],
+        from: 'test@flobrodemo.com',
+        subject: 'New Program Request',
+        templateId: 'd-d9f158ed05b44a799880e0765df62c57',
+        dynamic_template_data: body,
+    };
+    await sgMail.send(msg)
+    console.log('Email was sent...');
+
+    return null;
+}
